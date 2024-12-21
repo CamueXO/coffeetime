e70174ffb5472a6886b85708c68dc8268cb3a237
CamueXO <pupa@naver.com>
2024-12-21 19:07:52 +0900

    서버 로그, 재시도 코드 추가

--- test_server/index.js
+++ test_server/index.js
@@ -12,16 +12,25 @@ const httpsAgent = new https.Agent({
     rejectUnauthorized: false,
 });
 
+app.use('/static', express.static(path.join(__dirname)));
+
 app.get('/images', async (req, res) => {
     const targetUrl = 'https://www.hanwha701.com';
     try {
         const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            headless: true,
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--disable-accelerated-2d-canvas', '--disable-gpu'],
         });
         const page = await browser.newPage();
-        await page.goto(targetUrl, { waitUntil: 'networkidle2' });
+        await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
 
-        // 첫 번째 이미지를 다운로드
+        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+        page.on('requestfailed', request => {
+            console.log(`Request failed: ${request.url()} - ${request.failure().errorText}`);
+        });
+
+        await page.goto(targetUrl, { waitUntil: 'load', timeout: 90000 });
+
         const imgSrc = await page.evaluate(() => {
             const img = document.querySelector('img');
             return img ? img.src : null;
@@ -30,12 +39,14 @@ app.get('/images', async (req, res) => {
         if (imgSrc) {
             const viewSource = await page.goto(imgSrc);
             const buffer = await viewSource.buffer();
+            const imagePath = path.join(__dirname, 'downloaded_image.jpg');
 
-            fs.writeFile(path.join(__dirname, 'downloaded_image.jpg'), buffer, (err) => {
+            fs.writeFile(imagePath, buffer, (err) => {
                 if (err) {
                     console.error('Failed to save image:', err);
                     res.status(500).send('Failed to save image');
                 } else {
+                    console.log('Image saved at:', imagePath);
                     res.json({ message: 'Image saved successfully' });
                 }
             });
@@ -46,6 +57,8 @@ app.get('/images', async (req, res) => {
         await browser.close();
     } catch (error) {
         console.error('Failed to fetch image URLs:', error.message);
+        await fs.writeFile('error_html.html', await page.content());
+        await page.screenshot({ path: 'error_screenshot.png' });
         res.status(500).send('Failed to fetch image URLs');
     }
 });
